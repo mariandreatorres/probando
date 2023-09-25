3b26677dac7a196927287d61f194c989747f076b
Mariana Torres <mariandreatorres@gmail.com>
2023-09-24 22:26:57 -0300

    version 2: funciones y datos

---
+++ cumple.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def calcular_cumple(anio,mes,dia):
+    tiempo_actual = datetime.now()
+    micumple = datetime(anio,mes,dia)
+    tiempofalta = micumple-tiempo_actual
+    resultado = tiempofalta.days*60*60*24+tiempofalta.seconds
+    return resultado
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from datetime import datetime
-tiempo_actual = datetime.now()
-micumple = datetime(2023,9,27)
-tiempofalta = micumple-tiempo_actual
-print(tiempofalta.days*60*60*24+tiempofalta.seconds)
+from cumple import calcular_cumple
+
+segundos = calcular_cumple(2023,9,27)
+print("Los segundos que faltan para tu cumpleaños: " ,segundos)
+
